d472ac11a4df009af4be3595ff284ec0c759fdc5
luismorellb <luismorellbrbn0805@gmail.com>
2026-09-08 13:56:11 -0700

    Branch operaciones básicas creada

--- operaciones.py
+++ operaciones.py
@@ -1,15 +1,5 @@
-def calcular(a, b, operador):
-    if operador == "+":
-        return a + b
-    elif operador == "-":
-        return a - b
-    else:
-        return "Operador no válido"
+def sumar(a, b): 
+    return a + b 
 
-num1 = float(input("Ingresa el primer número: "))
-operador = input("Ingresa la operación (+ o -): ")
-num2 = float(input("Ingresa el segundo número: "))
-
-resultado = calcular(num1, num2, operador)
-
-print("Resultado:", resultado)
+def restar(a, b): 
+    return a - b
