d16d6d97402e221b5cc49e5157d1f87f7b254cfb
luismorellb <luismorellbrbn0805@gmail.com>
2026-09-14 12:25:11 -0700

    Add test for restar function

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,11 +1,9 @@
-from operaciones import sumar, multiplicar, dividir
-
+from operaciones import sumar, multiplicar, dividir, restar
 
 def test_sumar():
     #En este caso de prueba, verificamos que el resultado obtenido
     #de la funcion sea igual al resultado esperado que es 13
     assert sumar(6, 7) == 13
-
 
 def test_mult():
     #En este caso de prueba, verificamos que el resultado obtenido
@@ -16,3 +14,8 @@ def test_dividir():
     #En este caso de prueba, verificamos que el resultado obtenido
     #de la funcion sea igual al resultado esperado que es 2
     assert dividir(6, 3) == 2
+
+def test_restar():
+    #En este caso de prueba, verificamos que el resultado obtenido
+    #de la funcion sea igual al resultado esperado que es 5
+    assert restar(10, 5) == 5    
